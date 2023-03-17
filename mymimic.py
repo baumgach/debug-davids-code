b3fb172f5bbcba483feed62763e48950066df949
Christian Baumgartner <c.f.baumgartner@gmail.com>
2023-03-17 18:56:46 +0100

    added mymimic

---
+++ mymimic.py
@@ -0,0 +1,139 @@
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+import torchvision.transforms as tfs
+import cv2
+from PIL import Image
+import pandas as pd
+import os
+
+class MIMIC_Dataset(Dataset):
+    """MIMIC-CXR Dataset
+    Johnson AE, Pollard TJ, Berkowitz S, Greenbaum NR, Lungren MP, Deng CY, Mark RG, Horng S.
+    MIMIC-CXR: A large publicly available database of labeled chest radiographs.
+    arXiv preprint arXiv:1901.07042. 2019 Jan 21.
+    https://arxiv.org/abs/1901.07042
+    Dataset website here:
+    https://physionet.org/content/mimic-cxr-jpg/2.0.0/
+    """
+
+    def __init__(self,
+                 imgpath,
+                 csvpath,
+                 metacsvpath,
+                 views=["PA"],
+                 transform=None,
+                 data_aug=None,
+                 flat_dir=True,
+                 seed=0,
+                 unique_patients=True
+                 ):
+
+        super(MIMIC_Dataset, self).__init__()
+        np.random.seed(seed)  # Reset the seed so all runs are the same.
+
+        self.pathologies = ["Enlarged Cardiomediastinum",
+                            "Cardiomegaly",
+                            "Lung Opacity",
+                            "Lung Lesion",
+                            "Edema",
+                            "Consolidation",
+                            "Pneumonia",
+                            "Atelectasis",
+                            "Pneumothorax",
+                            "Pleural Effusion",
+                            "Pleural Other",
+                            "Fracture",
+                            "Support Devices"]
+
+        self.pathologies = sorted(self.pathologies)
+
+        self.imgpath = imgpath
+        self.transform = transform
+        self.data_aug = data_aug
+        self.csvpath = csvpath
+        self.csv = pd.read_csv(self.csvpath)
+        self.metacsvpath = metacsvpath
+        self.metacsv = pd.read_csv(self.metacsvpath)
+
+        self.csv = self.csv.set_index(['subject_id', 'study_id'])
+        self.metacsv = self.metacsv.set_index(['subject_id', 'study_id'])
+
+        self.csv = self.csv.join(self.metacsv).reset_index()
+
+        # Keep only the desired view
+        self.csv["view"] = self.csv["ViewPosition"]
+        self.limit_to_selected_views(views)
+
+        if unique_patients:
+            self.csv = self.csv.groupby("subject_id").first().reset_index()
+
+        # Get our classes.
+        healthy = self.csv["No Finding"] == 1
+        self.labels = []
+        for pathology in self.pathologies:
+            if pathology in self.csv.columns:
+                self.csv.loc[healthy, pathology] = 0
+                mask = self.csv[pathology]
+
+            self.labels.append(mask.values)
+        self.labels = np.asarray(self.labels).T
+        self.labels = self.labels.astype(np.float32)
+
+        # Make all the -1 values into nans to keep things simple
+        self.labels[self.labels == -1] = np.nan
+
+        # Rename pathologies
+        self.pathologies = np.char.replace(self.pathologies, "Pleural Effusion", "Effusion")
+
+        # add consistent csv values
+
+        # offset_day_int
+        self.csv["offset_day_int"] = self.csv["StudyDate"]
+
+        # patientid
+        self.csv["patientid"] = self.csv["subject_id"].astype(str)
+
+    def string(self):
+        return self.__class__.__name__ + " num_samples={} views={} data_aug={}".format(len(self), self.views, self.data_aug)
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        sample = {}
+        sample["idx"] = idx
+        sample["lab"] = self.labels[idx]
+
+        subjectid = str(self.csv.iloc[idx]["subject_id"])
+        studyid = str(self.csv.iloc[idx]["study_id"])
+        dicom_id = str(self.csv.iloc[idx]["dicom_id"])
+
+        img_path = os.path.join(self.imgpath, "p" + subjectid[:2], "p" + subjectid, "s" + studyid, dicom_id + ".jpg")
+        image = cv2.imread(img_path, 0)
+        image = Image.fromarray(image)
+        if self.mode == "train":
+            if self.transforms is None:
+                image = self.image_augmentation(image)
+            else:
+                image = self.transforms(image)
+        image = np.array(image)
+        image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+
+        # resize and normalize; e.g., ToTensor()
+        image = cv2.resize(
+            image,
+            dsize=(self.image_size, self.image_size),
+            interpolation=cv2.INTER_LINEAR,
+        )
+        image = image / 255.0
+        __mean__ = np.array([[[0.485, 0.456, 0.406]]])
+        __std__ = np.array([[[0.229, 0.224, 0.225]]])
+        image = (image - __mean__) / __std__
+        image = image.transpose((2, 0, 1)).astype(np.float32)
+        if self.class_index != -1:  # multi-class mode
+            label = np.array(self._labels_list[idx]).reshape(-1).astype(np.float32)
+        else:
+            label = np.array(self._labels_list[idx]).reshape(-1).astype(np.float32)
+
+        return image, label
